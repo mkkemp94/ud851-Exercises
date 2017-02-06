11fb3e68140266d08e31338dbe4e412f36e0135f
Kemp <kempm@wit.edu>
2017-02-05 22:06:45 -0500

    Recycler View On Click Listener
    Explicit Intents

--- Lesson03-Green-Recycler-View/T03.03-Exercise-RecyclerViewAdapter/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.03-Exercise-RecyclerViewAdapter/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -15,7 +15,9 @@
  */
 package com.example.android.recyclerview;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -33,20 +35,63 @@ import android.widget.TextView;
  * contents are green.
  */
 // TODO (4) From GreenAdapter, extend RecyclerView.Adapter<NumberViewHolder>
-public class GreenAdapter {
+public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder>{
 
     // TODO (1) Add a private int variable called mNumberItems
+    private int mNumberItems;
 
     // TODO (2) Create a constructor for GreenAdapter that accepts an int as a parameter for numberOfItems
     // TODO (3) Store the numberOfItems parameter in mNumberItems
+    public GreenAdapter(int numberOfItems) {
+
+        mNumberItems = numberOfItems;
+    }
 
     // TODO (5) Override the onCreateViewHolder method
     // TODO (6) Create and return a new NumberViewHolder within this method
 
+    @Override
+    public NumberViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // Get context to create a LayoutInflator
+        Context context = parent.getContext();
+
+        // Get the layout to inflate
+        int layoutId = R.layout.number_list_item;
+
+        // Not sure
+        boolean attachToParentImmediately = false;
+
+        // Make inflator from context
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Create view from inflating the layout
+        View view = inflater.inflate(layoutId, parent, attachToParentImmediately);
+
+        // Pass this view onto a View Holder
+        NumberViewHolder numberViewHolder = new NumberViewHolder(view);
+
+        // Return the view holder
+        return numberViewHolder;
+    }
+
+
     // TODO (7) Override onBindViewHolder
     // TODO (8) Within onBindViewHolder, call holder.bind and pass in the position
 
+    @Override
+    public void onBindViewHolder(NumberViewHolder holder, int position) {
+
+        // Update the item at this position (I think)
+        holder.bind(position);
+    }
+
+
     // TODO (9) Override getItemCount and return the number of items to display
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
 
     /**
      * Cache of the children views for a list item.
--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -53,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
+
                 // TODO (1) Retrieve the text from the EditText and store it in a variable
+                String name = mNameEntry.getText().toString().trim();
 
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
@@ -74,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
                 // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, name);
 
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
